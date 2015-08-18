8aa97c220e306da6a9772cd254e0a8d6d7f67c9d
Yaroslav Kurilin <norman_zp@outlook.com>
2015-08-18 11:11:54 +0300

    final
    Signed-off-by: Yaroslav Kurilin <norman_zp@outlook.com>

--- js/function.js
+++ js/function.js
@@ -5,7 +5,7 @@ $('.gallery_plate:nth-of-type(4n)').css
 		'margin-right': '0'
 	});
 //remove the margin-right at 3-el div in sidebar
-$('.sidebar .bar:nth-of-type(3n)').css
+$('.footer_inner .bar:nth-of-type(3n)').css
 	({
 		'margin-right': '0'
 	});
